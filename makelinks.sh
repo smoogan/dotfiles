d02e73939b93d8d1fb662213c6d2ee10b49c563a
Sean Moogan <sean.b.moogan@gmail.com>
2017-07-15 13:32:13 -0400

    Initial setup script which creates links from dotfiles to their correct system location.

---
+++ makelinks.sh
@@ -0,0 +1,8 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# VIM
+ln -fs $DIR/vim/.vimrc $HOME/.vimrc
+
+# i3wm
+ln -fs $DIR/i3/i3blocks.conf $HOME/.config/i3/i3blocks.conf
+ln -fs $DIR/i3/config $HOME/.config/i3/config
