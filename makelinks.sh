f417bd676693c1fdf6632e2960c50af5fad0116b
Sean Moogan <sean.b.moogan@gmail.com>
2017-12-04 10:57:21 -0500

    Autolinking for tmux conf

--- makelinks.sh
+++ makelinks.sh
@@ -6,3 +6,6 @@ ln -fs $DIR/vim/.vimrc $HOME/.vimrc
 # i3wm
 ln -fs $DIR/i3/i3blocks.conf $HOME/.config/i3/i3blocks.conf
 ln -fs $DIR/i3/config $HOME/.config/i3/config
+
+# tmux
+ln -fs $DIR/tmux/.tmux.conf $HOME/.tmux.conf
