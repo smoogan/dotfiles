a3b73907f5fc7a6fd21ff985a87cedec3664d488
Sean Moogan <sean.b.moogan@gmail.com>
2026-06-06 13:14:32 -0400

    Add lualine.nvim to configure statusline

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -22,6 +22,22 @@ return {
         "preservim/nerdtree",
     },
     {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            icons_enabled = true,
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {'filename'},
+                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
+            },
+
+        },
+    },
+    {
         "mason-org/mason.nvim",
         opts = {}
     },
